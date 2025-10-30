545a933e9a0764a2142345c339068e6d47edd507
FahadNawazCs <fahadnawaz.cs99@gmail.com>
2025-10-30 16:50:09 -0500

    Fixed RPS logic

--- src/main/java/RPSLogic.java
+++ src/main/java/RPSLogic.java
@@ -2,39 +2,39 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.*;
 
-public class RPSLogic{
+public class RPSLogic {
 
-    public void Logic() {
-    String[] choices = {"rock", "paper", "scissors"};
+    public static String[] choices = {"rock", "paper", "scissors"};
+    public String validateInput() {
 
-    Scanner input = new Scanner(System.in);
-    System.out.println("Enter your choice: (rock,paper,scissors): ");
-    String playerChoice = input.nextLine().toLowerCase();
 
-    if(!playerChoice.equals("rock") && !playerChoice.equals("paper") && !playerChoice.equals("scissors")) {
-        System.out.println("Enter one of the choices above: ");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your choice: (rock,paper,scissors): ");
+        String playerChoice = input.nextLine().toLowerCase();
+
+        if (!playerChoice.equals("rock") && !playerChoice.equals("paper") && !playerChoice.equals("scissors")) {
+            System.out.println("Enter one of the choices above: ");
+        }
+        return playerChoice;
     }
-
-    Random random = new Random();
-    int index = random.nextInt(3);
-    String computerChoice = choices[index];
-    System.out.println("Computer chose: " + computerChoice);
-    if(playerChoice.equals(computerChoice))
-    {
-        System.out.println("Its a draw!");
-    }
-    else if(  (playerChoice.equals("rock") && computerChoice.equals("scissors"))
-            || (playerChoice.equals("paper") && computerChoice.equals("rock"))
-            || (playerChoice.equals("scissors") && computerChoice.equals("paper"))
-            )
-    {
-        System.out.println("Congrats you win!");
-    }
-    else
-    {
-        System.out.println("Computer wins!");
-    }
-    }
-
-
 }
+
+    public String validateCOmputer(){
+        Random random = new Random();
+        int index = random.nextInt(3);
+        return RPSLogic.choices[index];
+        }
+
+void main() {
+}
+
+
+
+
+
+
+
+
+
+
+
