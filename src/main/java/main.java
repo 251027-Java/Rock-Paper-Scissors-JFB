2c4cb751f1e08c39b936889702ac859bc9c4a3e9
zhchgatech <zhchgatech@gmail.com>
2025-10-31 10:14:07 -0500

    Jen's main

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,41 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
+        String[] choices = {"rock", "paper", "scissors"};
+        int[] scores = {0, 0, 0};
+
+        boolean continuePlay = true;
+
+        while (continuePlay) {
+            try {
+                String choice = scanner.nextLine().toLowerCase();
+
+                if (choice.equals("quit")) {
+                    continuePlay = false;
+                    break;
+                }
+
+                if (!choice.equals("rock") ||choice.equals("paper")||choice.equals("scissors")) {
+                    System.out.println("Invalid input, please input the correct input:");
+                    continue;
+                }
+
+                String machineChoice = choices[rand.nextInt(3)];
+
+                String winner = RockPaperScissorsLogic.RPSResult(choice, machineChoice);
+
+
+            }
+            catch (Exception e) {
+                System.out.println("error: " + e.getMessage());
+            }
+            System.out.printf("You: %d | Computer: %d | Ties: %d%n", scores[0], scores[1], scores[2]);
+
+        }
+        scanner.close();
     }
 }
